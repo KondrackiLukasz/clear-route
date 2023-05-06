5f9f610e74b077a5e48fa4e802281d0d104e6a6d
lukasz <potrix3@gmail.com>
2023-05-06 13:30:46 +0200

    Extract routing component

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,51 +1,15 @@
-import { useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
-import { LatLngTuple } from 'leaflet';
+import {MapContainer, Marker, TileLayer} from 'react-leaflet';
+import {LatLngTuple} from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
 import 'leaflet-routing-machine';
 import 'fontawesome-free/css/all.min.css';
+import {Routing} from "./Routing.tsx";
 
-interface MapComponentProps {
+export interface MapComponentProps {
     from: LatLngTuple;
     to: LatLngTuple;
 }
 
-function Routing({ from, to }: MapComponentProps) {
-    const map = useMap();
-
-    useEffect(() => {
-        const routingControl = L.Routing.control({
-            waypoints: [L.latLng(from[0], from[1]), L.latLng(to[0], to[1])],
-            routeWhileDragging: false,
-            showAlternatives: false,
-            lineOptions: {
-                extendToWaypoints: true,
-                missingRouteTolerance: 0.0,
-                styles: [
-                    { color: 'blue', opacity: 0.6, weight: 4 },
-                    { color: 'blue', opacity: 0.6, weight: 4 },
-                    { color: 'blue', opacity: 0.6, weight: 4 },
-                ],
-            },
-            altLineOptions: {
-                extendToWaypoints: true,
-                missingRouteTolerance: 0.0,
-                styles: [
-                    { color: 'blue', opacity: 0.3, weight: 4 },
-                    { color: 'blue', opacity: 0.3, weight: 4 },
-                    { color: 'blue', opacity: 0.3, weight: 4 },
-                ],
-            },
-        }).addTo(map);
-
-        return () => {
-            map.removeControl(routingControl);
-        };
-    }, [map, from, to]);
-
-    return null;
-}
 export function MapComponent({ from, to }: MapComponentProps) {
     const zoom = 13;
 
---
+++ src/Routing.tsx
@@ -0,0 +1,40 @@
+import {useMap} from "react-leaflet";
+import {useEffect} from "react";
+import L from "leaflet";
+import {MapComponentProps} from "./Map.tsx";
+
+export function Routing({from, to}: MapComponentProps) {
+    const map = useMap();
+
+    useEffect(() => {
+        const routingControl = L.Routing.control({
+            waypoints: [L.latLng(from[0], from[1]), L.latLng(to[0], to[1])],
+            routeWhileDragging: false,
+            showAlternatives: false,
+            lineOptions: {
+                extendToWaypoints: true,
+                missingRouteTolerance: 0.0,
+                styles: [
+                    {color: 'blue', opacity: 0.6, weight: 4},
+                    {color: 'blue', opacity: 0.6, weight: 4},
+                    {color: 'blue', opacity: 0.6, weight: 4},
+                ],
+            },
+            altLineOptions: {
+                extendToWaypoints: true,
+                missingRouteTolerance: 0.0,
+                styles: [
+                    {color: 'blue', opacity: 0.3, weight: 4},
+                    {color: 'blue', opacity: 0.3, weight: 4},
+                    {color: 'blue', opacity: 0.3, weight: 4},
+                ],
+            },
+        }).addTo(map);
+
+        return () => {
+            map.removeControl(routingControl);
+        };
+    }, [map, from, to]);
+
+    return null;
+}
