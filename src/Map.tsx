3294d2c5dbce01e381624141519aafa7b18bb8cc
lukasz <potrix3@gmail.com>
2023-05-06 13:11:07 +0200

    First badly working routing

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,15 +1,53 @@
-import { MapContainer, TileLayer, Polyline, Marker } from 'react-leaflet';
-import { LatLngExpression, LatLngTuple } from 'leaflet';
+import { useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
+import { LatLngTuple } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import 'leaflet-routing-machine';
+import 'fontawesome-free/css/all.min.css';
 
 interface MapComponentProps {
     from: LatLngTuple;
     to: LatLngTuple;
 }
 
+function Routing({ from, to }: MapComponentProps) {
+    const map = useMap();
+
+    useEffect(() => {
+        const routingControl = L.Routing.control({
+            waypoints: [L.latLng(from[0], from[1]), L.latLng(to[0], to[1])],
+            routeWhileDragging: false,
+            showAlternatives: true,
+            lineOptions: {
+                extendToWaypoints: true,
+                missingRouteTolerance: 0.0,
+                styles: [
+                    { color: 'blue', opacity: 0.6, weight: 4 },
+                    { color: 'blue', opacity: 0.6, weight: 4 },
+                    { color: 'blue', opacity: 0.6, weight: 4 },
+                ],
+            },
+            altLineOptions: {
+                extendToWaypoints: true,
+                missingRouteTolerance: 0.0,
+                styles: [
+                    { color: 'blue', opacity: 0.3, weight: 4 },
+                    { color: 'blue', opacity: 0.3, weight: 4 },
+                    { color: 'blue', opacity: 0.3, weight: 4 },
+                ],
+            },
+        }).addTo(map);
+
+        return () => {
+            map.removeControl(routingControl);
+        };
+    }, [map, from, to]);
+
+    return null;
+}
 export function MapComponent({ from, to }: MapComponentProps) {
     const zoom = 13;
-    const route: LatLngExpression[] = [from, to];
 
     return (
         <MapContainer center={from} zoom={zoom} style={{ height: '100%', width: '100%' }}>
@@ -17,10 +55,9 @@ export function MapComponent({ from, to }: MapComponentProps) {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Polyline positions={route} pathOptions={{ color: 'blue' }} />
-            {route.map((point, index) => (
-                <Marker position={point} key={index} />
-            ))}
+            <Marker position={from} />
+            <Marker position={to} />
+            <Routing from={from} to={to} />
         </MapContainer>
     );
 }
