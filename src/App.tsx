2f33d331940f6d4a8e0b899867abe2ad2edd6c6e
lukasz <potrix3@gmail.com>
2023-05-06 13:00:55 +0200

    Change coordinates to gdynia and gdansk wrzeszcz

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import mapStyles from './map.module.css';
-import {MapComponent} from "./Map.tsx";
+import { MapComponent } from './Map.tsx';
 
 export function App() {
+    const gdanskWrzeszczCoordinates: [number, number] = [54.3842, 18.5922];
+    const gdyniaCoordinates: [number, number] = [54.5189, 18.5305];
+
     return (
         <div className={mapStyles.mapContainer}>
-            <MapComponent />
+            <MapComponent from={gdanskWrzeszczCoordinates} to={gdyniaCoordinates} />
         </div>
     );
 }
