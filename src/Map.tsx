242ead7554b17e307949437253a962a32da20def
lukasz <potrix3@gmail.com>
2023-05-09 19:12:37 +0200

    fix icon

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,5 +1,5 @@
 import {MapContainer, TileLayer} from 'react-leaflet';
-import {LatLngTuple, Icon} from 'leaflet';
+import {LatLngTuple} from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-routing-machine';
 import 'fontawesome-free/css/all.min.css';
@@ -12,13 +12,6 @@ export interface MapComponentProps {
 
 export function MapComponent({ from, to }: MapComponentProps) {
     const zoom = 13;
-
-    // Fix the default icon URLs
-    const defaultIcon = new Icon.Default();
-    defaultIcon.options.iconUrl = "images/marker-icon.png";
-    defaultIcon.options.shadowUrl = "images/marker-shadow.png";
-    Icon.Default.prototype.options.iconUrl = defaultIcon.options.iconUrl;
-    Icon.Default.prototype.options.shadowUrl = defaultIcon.options.shadowUrl;
 
     return (
         <MapContainer center={from} zoom={zoom} style={{ height: '100%', width: '100%' }}>
