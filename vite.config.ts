716c81d8e0e7785b85bafbb5c2d470b920fee508
lukasz <potrix3@gmail.com>
2023-05-11 18:20:34 +0200

    Please please please I want icons to work

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'clear-route',
+  base: '/clear-route/',
   plugins: [react()],
+  build: {
+    minify: false,
+    sourcemap: true,
+    target: 'esnext',
+  },
 })
