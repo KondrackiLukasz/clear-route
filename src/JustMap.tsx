eb78e40550ca09074da92fe0a8522b9de4315948
Lukasz Kondracki <lukasz.kondracki@dynatrace.com>
2023-05-22 20:31:22 +0200

    Comment out unused component

--- src/JustMap.tsx
+++ src/JustMap.tsx
@@ -1,39 +1,39 @@
-import {MapComponent} from "./Map.tsx";
-import * as React from "react";
-import {Station} from "./backend/useAllStations.ts";
-import {useNearStations} from "./backend/useNearStations.ts";
-import {useStationData} from "./backend/useStationData.ts";
-
-const initialLonFrom = 54.3842;
-const initialLatFrom = 18.5922;
-const initialLonTo = 54.5189;
-const initialLatTo = 18.5305;
-
-export function JustMap() {
-    const [lonFrom, setLonFrom] = React.useState(initialLonFrom);
-    const [latFrom, setLatFrom] = React.useState(initialLatFrom);
-    const [lonTo, setLonTo] = React.useState(initialLonTo);
-    const [latTo, setLatTo] = React.useState(initialLatTo);
-
-
-    const stations: Station[] = useNearStations([[lonFrom, latFrom], [lonTo, latTo]]);
-    const stationsData = useStationData(stations);
-
-    return (
-        <MapComponent
-        from={[lonFrom, latFrom]}
-        to={[lonTo, latTo]}
-        setFrom={(L: React.SetStateAction<number>[]) => {
-            setLatFrom(L[0]);
-            setLonFrom(L[1]);
-        }}
-        setTo={(L: React.SetStateAction<number>[]) => {
-            setLatTo(L[0]);
-            setLonTo(L[1]);
-        }}
-        stations={stations}
-        stationsData = {stationsData}
-        toolbarVisible = {false}
-        selectedDate={new Date()}/>
-    )
-}
+// import {MapComponent} from "./Map.tsx";
+// import * as React from "react";
+// import {Station} from "./backend/useAllStations.ts";
+// import {useNearStations} from "./backend/useNearStations.ts";
+// import {useStationData} from "./backend/useStationData.ts";
+//
+// const initialLonFrom = 54.3842;
+// const initialLatFrom = 18.5922;
+// const initialLonTo = 54.5189;
+// const initialLatTo = 18.5305;
+//
+// export function JustMap() {
+//     const [lonFrom, setLonFrom] = React.useState(initialLonFrom);
+//     const [latFrom, setLatFrom] = React.useState(initialLatFrom);
+//     const [lonTo, setLonTo] = React.useState(initialLonTo);
+//     const [latTo, setLatTo] = React.useState(initialLatTo);
+//
+//
+//     const stations: Station[] = useNearStations([[lonFrom, latFrom], [lonTo, latTo]]);
+//     const stationsData = useStationData(stations);
+//
+//     return (
+//         <MapComponent
+//         from={[lonFrom, latFrom]}
+//         to={[lonTo, latTo]}
+//         setFrom={(L: React.SetStateAction<number>[]) => {
+//             setLatFrom(L[0]);
+//             setLonFrom(L[1]);
+//         }}
+//         setTo={(L: React.SetStateAction<number>[]) => {
+//             setLatTo(L[0]);
+//             setLonTo(L[1]);
+//         }}
+//         stations={stations}
+//         stationsData = {stationsData}
+//         toolbarVisible = {false}
+//         selectedDate={new Date()}/>
+//     )
+// }
